a6483bc8db81079f3e5b2428faecde889523a4e0
João Guilherme <joaosantos34537445n@gmail.com>
2024-07-17 09:25:36 -0300

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,18 @@
+#' @title pensiondatacheck: Efficient and Effective Critical Analysis of Pension Fund Data.
+#' @description
+#' A short description...
+#' @param x description
+#' @return The output from \code{\link{print}}
+#' @export
+#'
+#' @import readxl
+#' @examples
+#' hello("John")
+#' \dontrun{
+#' hello("Steve")
+#'
+#' }
+hello <- function(x) {
+  print(paste0("Hello ", x," world!"))
+}
+
